3e2e88b195e598647c07d269bd073057c04bd3a2
Bekzod <bekzodturgunoff@gmail.com>
2022-11-30 14:16:06 +0500

    javascript lesson started

---
+++ dist/index.dev.js
@@ -0,0 +1,74 @@
+"use strict";
+
+/*
+undefined, null, boolen, string, symbol, number, object
+*/
+// o'zgaruvchi yaratish
+var a; // console.log(a);
+//o'zgaruvchini data type bilan tenglashtirish
+
+var b = 8; // console.log(b);
+//oz'garuvchiga data type berish
+
+a = 10; // console.log(a);
+// b o'zgaruvchini data type ni a o'zgaruvchining data type ga o'zgartirdik
+
+b = a; // console.log(b);
+////////////
+
+var w = 5;
+var e = 10;
+var r = "Men"; ///////////
+// Birinchi usul
+
+w = w + 1;
+e = e + 5;
+r = r + "text man"; //Ikkinchi usul
+
+w += 1;
+e += 5;
+r += "qo'shildi"; /////////////////
+
+var buNomer;
+var buText;
+var buBall;
+BUNOMER = 10;
+BUTEXT = "text";
+BUBALL = 3999; ///////////////
+// JavaScriptda nomerlarni bir birga qo'shish
+
+var sum = 10 + 10; // JavaScriptda nomerlarni bir birdan ayrish
+
+var vallue = 20 - 10; // JavaScriptda nomerlarni bir birga ko'paytirish
+
+var product = 2 * 2; // JavaScriptda nomerlarni bir birga bo'lish
+
+var server = 6 / 2;
+/* 
+
++=
+-=
+*=
+/=
+
+*/
+//  JavaScriptda nomer qo'shish
+
+var meningNomerim = 30; // qancha son qo'shmoqchi bo'lsangiz pastdagi code ga qo'shing
+// Birinchi usul
+
+meningNomerim = meningNomerim + 1; // Ikkinchi usul
+
+meningNomerim++; //  JavaScriptda nomer ayirish
+
+var meningNomerimIkkinchi = 40; // Birinchi usul
+
+meningNomerimIkkinchi = meningNomerimIkkinchi - 1; // Ikkinchi usul
+
+meningNomerimIkkinchi--; // JavaScript da kasr son yaratish
+
+var bizningKasrSon = 5.7; ////////////////////
+
+var meningKasrim = 0.009; ////////////
+
+var kasrSon = 2.5 + 2.0; /////////////
---
+++ index.js
@@ -0,0 +1,118 @@
+/*
+undefined, null, boolen, string, symbol, number, object
+*/
+
+// o'zgaruvchi yaratish
+
+let a;
+// console.log(a);
+
+//o'zgaruvchini data type bilan tenglashtirish
+
+let b = 8;
+// console.log(b);
+//oz'garuvchiga data type berish
+
+a = 10;
+// console.log(a);
+// b o'zgaruvchini data type ni a o'zgaruvchining data type ga o'zgartirdik
+
+b = a;
+
+// console.log(b);
+
+////////////
+
+let w = 5;
+let e = 10;
+let r = "Men";
+
+///////////
+
+// Birinchi usul
+w = w + 1;
+e = e + 5;
+r = r + "text man";
+
+//Ikkinchi usul
+
+w += 1;
+e += 5;
+r += "qo'shildi";
+
+/////////////////
+
+let buNomer;
+
+let buText;
+
+let buBall;
+
+BUNOMER = 10;
+
+BUTEXT = "text";
+
+BUBALL = 3999;
+
+///////////////
+
+// JavaScriptda nomerlarni bir birga qo'shish
+
+let sum = 10 + 10;
+
+// JavaScriptda nomerlarni bir birdan ayrish
+
+let vallue = 20 - 10;
+
+// JavaScriptda nomerlarni bir birga ko'paytirish
+
+let product = 2 * 2;
+
+// JavaScriptda nomerlarni bir birga bo'lish
+
+let server = 6 / 2;
+
+/* 
+
++=
+-=
+*=
+/=
+
+*/
+
+//  JavaScriptda nomer qo'shish
+
+let meningNomerim = 30;
+
+// qancha son qo'shmoqchi bo'lsangiz pastdagi code ga qo'shing
+
+// Birinchi usul
+
+meningNomerim = meningNomerim + 1;
+
+// Ikkinchi usul
+
+meningNomerim++;
+
+//  JavaScriptda nomer ayirish
+
+let meningNomerimIkkinchi = 40;
+
+// Birinchi usul
+meningNomerimIkkinchi = meningNomerimIkkinchi - 1;
+
+// Ikkinchi usul
+meningNomerimIkkinchi--;
+
+// JavaScript da kasr son yaratish
+
+let bizningKasrSon = 5.7;
+
+////////////////////
+let meningKasrim = 0.009;
+
+////////////
+let kasrSon = 2.5 + 2.0;
+
+/////////////
